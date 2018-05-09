425797d4e1e1cbd2edeb6965e78696df10bb8170
Jeff Purcell <japurcell@users.noreply.github.com>
2018-05-09 08:35:20 -0700

    Adding thread safety to memoization functions

--- Analysis.UnitTests/LibTests.fs
+++ Analysis.UnitTests/LibTests.fs
@@ -19,6 +19,23 @@ let ``it should call a function with the result of the previous function.`` () =
     [one; two] |> should equal [1; 2]
 
 [<Test>]
+let ``it should call a function with the result of the previous function when multithreaded.`` () =
+    let adder = memoizePrev (Func<int, int>(fun prev -> prev + 1)) startingValue
+    let asyncAdder = async { return adder.Invoke() }
+
+    let asyncController =
+        asyncAdder
+        |> List.replicate 20
+        |> Async.Parallel
+
+    let result =
+        asyncController
+        |> Async.RunSynchronously
+        |> Seq.reduce (fun i0 i1 -> i0 + i1)
+
+    result |> should equal 210
+
+[<Test>]
 let ``it should return the result of the previous function call.`` () =
     let adder = memoizePrevPrev (Func<int, int, int>(fun prev _ -> prev + 1)) startingValue
 
@@ -26,3 +43,54 @@ let ``it should return the result of the previous function call.`` () =
     let two = adder.Invoke 0
 
     [one; two] |> should equal [0; 1]
+
+[<Test>]
+let ``it should return the result of the previous function call when multithreaded.`` () =
+    let adder = memoizePrevPrev (Func<int, int, int>(fun prev _ -> prev + 1)) startingValue
+    let asyncAdder = async { return adder.Invoke 0 }
+
+    let asyncController =
+        asyncAdder
+        |> List.replicate 20
+        |> Async.Parallel
+
+    let result =
+        asyncController
+        |> Async.RunSynchronously
+        |> Seq.reduce (fun i0 i1 -> i0 + i1)
+
+    result |> should equal 190
+
+[<Test>]
+let ``it should only call a cached function once.`` () =
+    let calls = ref 0
+    let adder = memo (Func<int, int>(fun i ->
+        incr calls
+        i + 1))
+
+    adder.Invoke 1 |> ignore
+    adder.Invoke 1 |> ignore
+
+    !calls |> should equal 1
+
+[<Test>]
+let ``it should only call a cached function once when multithreaded.`` () =
+    let calls = ref 0
+    let adder = memo (Func<int, int>(fun i ->
+        incr calls
+        i + 1))
+
+    let asyncAdder arg = async { return adder.Invoke arg }
+    let asyncController =
+        asyncAdder
+        |> List.replicate 20
+        |> List.mapi (fun index f -> f (match index % 2 with | 0 -> index | _ -> 0))
+        |> Async.Parallel
+
+    let result =
+        asyncController
+        |> Async.RunSynchronously
+        |> Seq.reduce (fun i0 i1 -> i0 + i1)
+
+    !calls |> should equal 10
+    result |> should equal 110
--- Psns.Common.Analysis/lib.fs
+++ Psns.Common.Analysis/lib.fs
@@ -2,17 +2,28 @@
 
 module Lib =
     open System
+    open System.Collections.Concurrent
+
+    /// <summary>Composes a function whose result is stored so that it is only executed once.</summary>
+    /// <param name="f">A function whose result will be stored after the first execution.</param>
+    /// <remarks>Threadsafe</remarks>
+    let memo (f: Func<'a, 'b>) =
+        let store = new ConcurrentDictionary<_, _>()
+        Func<'a, 'b>(fun x -> store.GetOrAdd(Some x, lazy (f.Invoke x)).Force())
 
     /// <summary>Composes a function that is called with the result of the previous call.</summary>
     /// <param name="func"></param>
-    /// <param name="starting">The first value to return</param>
+    /// <remarks>Threadsafe</remarks>
     let memoizePrev (func: Func<'a, 'a>) starting =
         let cached = ref starting
+        let guard = new obj()
         Func<'a>(
             fun () ->
-                let r = func.Invoke !cached
-                cached := r
-                r
+                lock(guard)(fun () ->
+                    let r = func.Invoke !cached
+                    cached := r
+                    r
+            )
         )
 
     /// <summary>Composes a function that is called with the result of the previous
@@ -20,12 +31,16 @@ module Lib =
     /// will be the previous result rather than the result of the current call.</summary>
     /// <param name="func"></param>
     /// <param name="starting">The first value to return</param>
+    /// <remarks>Threadsafe</remarks>
     let memoizePrevPrev (func: Func<'a, 'b, 'a>) starting =
         let cached = ref starting
+        let guard = new obj()
         Func<'b, 'a>(
             fun x ->
-                let next = func.Invoke (!cached, x)
-                let res = !cached
-                cached := next
-                res
+                lock(guard)(fun () ->
+                    let next = func.Invoke (!cached, x)
+                    let res = !cached
+                    cached := next
+                    res
+                )
         )
